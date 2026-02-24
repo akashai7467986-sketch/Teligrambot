8c1433127dc5ebfec70d9b7d824b57fefb5357fe
akashai7467986-sketch <akashai7467986@gmail.com>
2026-02-24 20:52:27 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install requirements
-pip install -r requirements.txt
-
-# Start bot with gunicorn (production server)
-gunicorn bot:app --bind 0.0.0.0:$PORT --workers 1 --threads 2 --timeout 120
